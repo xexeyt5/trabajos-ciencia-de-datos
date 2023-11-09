4eab6d195e962df1241dccdbb94549e105d2d20d
xexeyt <cbwyuren500@gmail.com>
2023-11-08 21:56:35 -0600

    insercion de video y rango de edad

--- mi_app.py
+++ mi_app.py
@@ -7,7 +7,17 @@ Hello *world!*
 """)
 st.write("Yuren joshua salgado martinez")
 st.write("no. control 1909141")
+st.write("semestre 9")
 
 
-df = pd.read_csv("datos.csv")
-st.line_chart(df)
+df = pd.read_csv("titanic_dataset.csv")
+#st.line_chart(df)
+
+# Agregar un control deslizante
+age = st.slider("Selecciona una edad", 0, 100, 30)
+
+#agrega un video mediante un link
+st.video("https://www.youtube.com/watch?v=TXSOitGoINE")
+
+#corre la pagina
+#streamlit run /home/xexeyt/Desktop/trabajos-ciencia-de-datos/mi_app.py
