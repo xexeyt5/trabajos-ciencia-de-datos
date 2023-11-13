55abd0b264294c10677a955c100129987266c133
xexeyt <cbwyuren500@gmail.com>
2023-11-13 15:30:19 -0600

    cambio de dataframe junto con una fila la grafica

--- mi_app.py
+++ mi_app.py
@@ -11,14 +11,14 @@ st.write("semestre 9")
 
 
 #se carga el dataframe y se visualiza en una grafica
-df = pd.read_csv("titanic_dataset.csv")
-st.line_chart(df)
+df = pd.read_csv("housing.csv")
+st.line_chart(df['population'])
 
 # Agregar un control deslizante
-age = st.slider("Selecciona una edad", 0, 100, 30)
+#age = st.slider("Selecciona una edad", 0, 100, 30)
 
 #agrega un video mediante un link
-st.video("https://www.youtube.com/watch?v=TXSOitGoINE")
+#st.video("https://www.youtube.com/watch?v=TXSOitGoINE")
 
 #corre la pagina
-#streamlit run /home/xexeyt/Desktop/trabajos-ciencia-de-datos/mi_app.py
+#streamlit run mi_app.py
